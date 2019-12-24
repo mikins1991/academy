18daf80693198a63d799a30d31a0d3bcc0d7d5cc
mikins1991 <52010686+mikins1991@users.noreply.github.com>
2019-12-24 15:59:50 +0300

    24.12

--- tasks/scriptThis.js
+++ tasks/scriptThis.js
@@ -4,16 +4,30 @@ let calculator = {
         this.a = +prompt('Число a:', '2');
         this.b = +prompt('Число b:', '3');
     },
-
     sum() {
         return this.a + this.b;
     },
-
     mul() {
         return this.a * this.b;
     }
 };
-
 calculator.read();
 alert(calculator.sum());
 alert(calculator.mul());
+
+// task2
+let ladder = {
+    step: 0,
+    up() {
+        this.step++;
+        return this;
+    },
+    down() {
+        this.step--;
+        return this;
+    },
+    showStep: function () {
+        alert(this.step);
+    }
+};
+ladder.up().up().down().showStep();
