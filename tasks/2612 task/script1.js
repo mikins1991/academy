c312db3f5eddea2e040af4fe03c6b885eb29e0bb
mikins1991 <mikins19911@gmail.com>
2019-12-27 23:18:40 +0300

    task1&dz

---
+++ tasks/2612 task/script1.js
@@ -0,0 +1,19 @@
+function accum(str) {
+    let result = '';
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < i + 1; j++) {
+            let letter = str[i];
+            if (j == 0) {
+                letter = letter.toUpperCase();
+            } else {
+                letter = letter.toLowerCase();
+            }
+            result += letter;
+        }
+        if (i != str.length - 1) {
+            result = result + '-';
+        }
+    }
+    return result;
+}
+console.log(accum("abcd"));
