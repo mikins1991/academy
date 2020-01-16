8f00de2fe1a08f0b1f9f961ab70e2cd98b406160
mikins1991 <52010686+mikins1991@users.noreply.github.com>
2020-01-16 17:26:53 +0300

    3

--- tasks/10_01 task/script.js
+++ tasks/10_01 task/script.js
@@ -1,20 +1,32 @@
-let addBtn = document.querySelector('#addBtnJs');
-addBtn.onclick = function () {
-    function color() {
-        return Math.floor(Math.random() * 256);
-    }
-    addBtn.style.background = `rgb(${color() + ',' + color() + ',' + color()})`;
+// let addBtn = document.querySelector('#addBtnJs');
+// addBtn.onclick = function () {
+//     function color() {
+//         return Math.floor(Math.random() * 256);
+//     }
+//     addBtn.style.background = `rgb(${color() + ',' + color() + ',' + color()})`;
 
-    let str = Math.random().toString(36).substring(2, 5);
-    addBtn.innerHTML = `${str}`;
-}
+//     let str = Math.random().toString(36).substring(2, 5);
+//     addBtn.innerHTML = `${str}`;
+// }
 
 let block = document.getElementsByClassName('block');
 for (i = 0; i < block.length; i++) {
     block[i].addEventListener('click', function (event) {
-
         if (event.target.className == "btnDelete") {
             event.currentTarget.remove();
         }
     });
 }
+
+let addBtn = document.querySelector('#addBtnJs');
+let addInput = document.querySelector('.addInput');
+
+addBtn.addEventListener('click', function (e) {
+    if (e.target.className == "addBtn") {
+        document.querySelector('.blockText').innerHTML = addInput.value;
+    }
+});
+
+// addBtn.onclick = function () {
+
+// }
