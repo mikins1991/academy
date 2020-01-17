6f801e38f431fbf8a10ac28b7d127d0d35f02314
mikins1991 <mikins19911@gmail.com>
2020-01-17 20:48:10 +0300

    final

--- tasks/10_01 task/script.js
+++ tasks/10_01 task/script.js
@@ -1,14 +1,6 @@
-// let block = document.getElementsByClassName('block');
-// for (i = 0; i < block.length; i++) {
-//     block[i].addEventListener('click', function (event) {
-//         if (event.target.className == "btnDelete") {
-//             event.currentTarget.remove();
-//         }
-//     });
-// }
-
 const MENU = document.querySelector('.menu');
 const CHECKNONE = document.getElementById('check');
+
 let addInput = document.querySelector('.addInput');
 let addBtn = document.querySelector('.addBtn');
 
@@ -19,14 +11,13 @@ MENU.addEventListener('click', function (event) {
     }
 });
 
-addBtn.addEventListener('click', function (e) {
-    if (e.target.className == "addBtn") {
+addBtn.addEventListener('click', function (event) {
+    if (event.target.className == "addBtn") {
         let menuList = MENU.firstElementChild;
         let listClone = menuList.cloneNode(MENU);
         MENU.appendChild(listClone).querySelector('.blockText').innerHTML = addInput.value;
     }
 });
-
 
 function checkFun() {
     if (CHECKNONE.checked == true) {
@@ -34,37 +25,20 @@ function checkFun() {
     } else {
         MENU.style.display = 'block';
     }
-}
+};
 
 let searchInput = document.querySelector('.searchInput');
-
 searchInput.oninput = function () {
-    let str = searchInput.value;
-    let textP = document.getElementsByClassName('blockText')
-    console.log(str);
+    let list = document.getElementsByClassName('list');
+    let str = searchInput.value.toLowerCase();
 
-    for (i = 0; i < textP.length; i++) {
-        let textPP = textP[i].innerHTML;
-        let position = textPP.indexOf(str);
+    for (i = 0; i < list.length; i++) {
+        let para = list[i].getElementsByTagName('p')[0];
+        let position = para.innerText.toLowerCase().indexOf(str);
         if (position !== -1) {
-            console.log('ok');
+            list[i].style.display = "";
         } else {
-            console.log('no')
+            list[i].style.display = "none";
         }
-        console.log(position);
     }
-}
-
-MENU.addEventListener('oninput', function (event) {
-    if (event.target.className == 'searchInput') {
-
-    }
-});
-// function searchFun(text, str) {
-//     if (text.search(str) != -1) {
-//         console.log('ok');
-//     } else {
-//         console.log('no');
-//     }
-//     console.log(str);
-// }
+};
