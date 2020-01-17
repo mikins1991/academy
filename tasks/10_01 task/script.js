9232584eec5aa2deacbf2b543ef24df4b094feca
mikins1991 <52010686+mikins1991@users.noreply.github.com>
2020-01-17 16:52:47 +0300

    3

--- tasks/10_01 task/script.js
+++ tasks/10_01 task/script.js
@@ -1,32 +1,70 @@
-// let addBtn = document.querySelector('#addBtnJs');
-// addBtn.onclick = function () {
-//     function color() {
-//         return Math.floor(Math.random() * 256);
-//     }
-//     addBtn.style.background = `rgb(${color() + ',' + color() + ',' + color()})`;
-
-//     let str = Math.random().toString(36).substring(2, 5);
-//     addBtn.innerHTML = `${str}`;
+// let block = document.getElementsByClassName('block');
+// for (i = 0; i < block.length; i++) {
+//     block[i].addEventListener('click', function (event) {
+//         if (event.target.className == "btnDelete") {
+//             event.currentTarget.remove();
+//         }
+//     });
 // }
 
-let block = document.getElementsByClassName('block');
-for (i = 0; i < block.length; i++) {
-    block[i].addEventListener('click', function (event) {
-        if (event.target.className == "btnDelete") {
-            event.currentTarget.remove();
-        }
-    });
-}
-
-let addBtn = document.querySelector('#addBtnJs');
+const MENU = document.querySelector('.menu');
+const CHECKNONE = document.getElementById('check');
 let addInput = document.querySelector('.addInput');
+let addBtn = document.querySelector('.addBtn');
 
-addBtn.addEventListener('click', function (e) {
-    if (e.target.className == "addBtn") {
-        document.querySelector('.blockText').innerHTML = addInput.value;
+MENU.addEventListener('click', function (event) {
+    if (event.target.className == 'btnDelete') {
+        const listDel = event.target.parentElement.parentElement;
+        listDel.remove();
     }
 });
 
-// addBtn.onclick = function () {
+addBtn.addEventListener('click', function (e) {
+    if (e.target.className == "addBtn") {
+        let menuList = MENU.firstElementChild;
+        let listClone = menuList.cloneNode(MENU);
+        MENU.appendChild(listClone).querySelector('.blockText').innerHTML = addInput.value;
+    }
+});
 
+
+function checkFun() {
+    if (CHECKNONE.checked == true) {
+        MENU.style.display = 'none';
+    } else {
+        MENU.style.display = 'block';
+    }
+}
+
+let searchInput = document.querySelector('.searchInput');
+
+searchInput.oninput = function () {
+    let str = searchInput.value;
+    let textP = document.getElementsByClassName('blockText')
+    console.log(str);
+
+    for (i = 0; i < textP.length; i++) {
+        let textPP = textP[i].innerHTML;
+        let position = textPP.indexOf(str);
+        if (position !== -1) {
+            console.log('ok');
+        } else {
+            console.log('no')
+        }
+        console.log(position);
+    }
+}
+
+MENU.addEventListener('oninput', function (event) {
+    if (event.target.className == 'searchInput') {
+
+    }
+});
+// function searchFun(text, str) {
+//     if (text.search(str) != -1) {
+//         console.log('ok');
+//     } else {
+//         console.log('no');
+//     }
+//     console.log(str);
 // }
