e84b33192bca24036e2654fbd0270dd7e00e0089
mikins1991 <52010686+mikins1991@users.noreply.github.com>
2020-01-30 12:00:39 +0300

    final

--- DZ-DOM/dz-date/script.js
+++ DZ-DOM/dz-date/script.js
@@ -1,15 +1,17 @@
-let inputDate = document.querySelector('.inputDate');
-let divBirth = document.querySelector('.divDate');
-inputDate.addEventListener('focusout', birthdayInput);
+const INPUTDATE = document.querySelector('.inputDate');
+const DIVDATE = document.querySelector('.divDate');
+INPUTDATE.addEventListener('focusout', birthdayInput);
 
 function birthdayInput() {
-    let date = new Date();
-    let dateBirth = new Date(Date.parse(inputDate.value));
-    let birth = dateBirth.getDate() - date.getDate();
-    let b = birth.setDate();
-    divBirth.innerText = dateBirth;
-    console.log(birth.setDate());
-    console.log(inputDate.value);
-    console.log(dateBirth);
-    console.log(date);
+    let dateNow = new Date();
+    let dateBirth = new Date(Date.parse(INPUTDATE.value));
+    let birthDayThisYear = new Date(dateNow.getFullYear(), dateBirth.getMonth(), dateBirth.getDate())
+    let daysToBirthday = Math.ceil((birthDayThisYear - dateNow) / (1000 * 60 * 60 * 24));
+    if (daysToBirthday >= 0) {
+        DIVDATE.innerText = `Next birthday in ${daysToBirthday} days`;
+    } else {
+        let birthDayLastYear = new Date(dateNow.getFullYear() + 1, dateBirth.getMonth(), dateBirth.getDate())
+        daysToBirthday = Math.ceil((birthDayLastYear - dateNow) / (1000 * 60 * 60 * 24));
+        DIVDATE.innerText = `Next birthday in ${daysToBirthday} days`;
+    }
 }
