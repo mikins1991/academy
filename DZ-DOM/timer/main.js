1cb23e349a26c4fadeb072377f112787e0c10568
mikins1991 <mikins19911@gmail.com>
2020-02-05 15:49:46 +0300

    0502

--- DZ-DOM/timer/main.js
+++ DZ-DOM/timer/main.js
@@ -1,4 +1,4 @@
-let timerId = setInterval(onIntervalNextTick, 100);
+let timerId = setInterval(onIntervalNextTick, 1000);
 let timeIdSet;
 
 const htmlElements = {};
@@ -9,39 +9,44 @@ htmlElements.clock = document.querySelector('.container .links .clock');
 htmlElements.stopwatch = document.querySelector('.container .links .stopwatch');
 htmlElements.timer = document.querySelector('.container .links .timer');
 htmlElements.output = document.querySelector('.container .output');
-htmlElements.links = document.querySelector('.links');
 htmlElements.select = document.querySelectorAll('.hidden')
+htmlElements.buttons = document.querySelectorAll(".hidden");
 
 htmlElements.startBtn.addEventListener('click', buttonStartWarch);
 
-htmlElements.links.addEventListener('click', function (event) {
-    let link = event.target;
-    if (link.className == 'clock selected' || link.className == 'black') {
-        link.className = 'blue';
-        htmlElements.stopwatch.className = 'stopwatch';
-        htmlElements.timer.className = 'timer';
+function showButtons() {
+    htmlElements.buttons.forEach(item => item.classList.remove('hidden'));
+}
 
-        clearInterval(timeIdSet);
-        resetStopWatch();
-        timerId = setInterval(onIntervalNextTick, 100);
+function hideButtons() {
+    htmlElements.buttons.forEach(item => item.classList.add('hidden'));
+}
 
-    } else if (link.className == 'stopwatch') {
-        link.className = 'blue';
-        htmlElements.timer.className = 'timer';
-        htmlElements.clock.className = 'black';
+htmlElements.stopwatch.onclick = function () {
+    showButtons();
+    htmlElements.stopwatch.classList.add('selected');
+    htmlElements.timer.classList.remove('selected');
+    htmlElements.clock.classList.remove('selected');
+    htmlElements.output.innerHTML = `${`0`}:${`00`}`;
+    clearInterval(timerId);
+}
 
-        clearInterval(timerId);
-        addStopwatch();
+htmlElements.clock.onclick = function () {
+    hideButtons();
+    htmlElements.clock.classList.add('selected');
+    htmlElements.timer.classList.remove('selected');
+    htmlElements.stopwatch.classList.remove('selected');
+    clearInterval(timeIdSet);
+    timerId = setInterval(onIntervalNextTick, 100);
+}
 
-    } else if (link.className == 'timer') {
-        link.className = 'blue';
-        htmlElements.clock.className = 'black';
-        htmlElements.stopwatch.className = 'stopwatch';
-
-        resetStopWatch();
-        clearInterval(timerId);
-    }
-})
+htmlElements.timer.onclick = function () {
+    hideButtons();
+    htmlElements.timer.classList.add('selected');
+    htmlElements.clock.classList.remove('selected');
+    htmlElements.stopwatch.classList.remove('selected');
+    clearInterval(timerId);
+}
 
 function onIntervalNextTick() {
     let date = new Date();
@@ -49,23 +54,10 @@ function onIntervalNextTick() {
     htmlElements.output.innerHTML = time;
 }
 
-function addStopwatch() {
-    htmlElements.select[0].className = 'start hiddenoff';
-    htmlElements.select[1].className = 'stop hiddenoff';
-    htmlElements.select[2].className = 'reset hiddenoff';
-    htmlElements.output.innerHTML = `${`0`}:${`00`}`;
-}
-
-function resetStopWatch() {
-    htmlElements.select[0].className = 'start hidden';
-    htmlElements.select[1].className = 'stop hidden';
-    htmlElements.select[2].className = 'reset hidden';
-}
-
 function buttonStartWarch() {
     const startTime = new Date().getTime();
-    timeIdSet = setInterval(Timer, 1000);
-    function Timer() {
+    timeIdSet = setInterval(timer, 1000);
+    function timer() {
         const difference = (new Date().getTime() - startTime) / 1000;
         let seconds = parseInt(difference % 60);
         const minutes = parseInt((difference / 60) % 60);
@@ -74,15 +66,15 @@ function buttonStartWarch() {
         }
         htmlElements.output.innerHTML = `${minutes}:${seconds}`;
     }
-
     htmlElements.stopBtn.addEventListener('click', buttonStopWath);
-    function buttonStopWath() {
-        clearInterval(timeIdSet);
-    }
-
     htmlElements.resetBtn.addEventListener('click', buttonResetWath);
-    function buttonResetWath() {
-        clearInterval(timeIdSet);
-        htmlElements.output.innerHTML = `${`0`}:${`00`}`;
-    }
+}
+
+function buttonStopWath() {
+    clearInterval(timeIdSet);
+}
+
+function buttonResetWath() {
+    clearInterval(timeIdSet);
+    htmlElements.output.innerHTML = `${`0`}:${`00`}`;
 }
